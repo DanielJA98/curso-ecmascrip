d77e8d9e63726462fc1ebd39a40b39dcbdf6796a
DanielJA98LDELLDeb <djimeneza7b2276@outlook.com>
2024-03-07 20:04:24 -0600

    clase de templete literals

---
+++ src/es64/03-strings.js
@@ -0,0 +1,17 @@
+let hello = "hola";
+let Wordl = "wordl";
+let epicphrase = hello + " " + Wordl + " !";
+
+
+
+//template literals
+let epicprase2 = `${hello} ${Wordl} !`;
+console.log(epicprase2);
+
+// salto de linea 
+let lorem = "primera parte de la linea \n "  + "segunda linea ";
+console.log(lorem);
+
+// templete literals utiliza las comillas francesas para poder ingnorar 
+//las funciones de la anterior funciones como los strings
+//salto de lina omitiendo la parte de /n
