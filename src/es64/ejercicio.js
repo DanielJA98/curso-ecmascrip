3a5f320945c1ed5c14e8315b399c15319e8423f7
DanielJA98LDELLDeb <djimeneza7b2276@outlook.com>
2024-03-19 19:20:08 -0600

    ejercicio clase 9

---
+++ src/es64/ejercicio.js
@@ -0,0 +1,8 @@
+function solution(json1 = { name: "Mr. Michi", food: "pescado" }, json2 = { age: 12, color: "Blanco" }) {
+    let json3 = {...json1, ...json2};
+    console.log(json3);
+  }
+  
+json 
+
+  solution(json1 ={name: "Bigotes", food: "pollito" },);
