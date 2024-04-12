429fd3a0b4971b5999689e1bf731f931d9a6634c
DanielJA98LDELLDeb <djimeneza7b2276@outlook.com>
2024-04-11 20:54:31 -0600

    es8 object entries y object values

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,5 @@
+const countries = {MX: "Mexico", CO: "colombia", CL: "Chile", PE: "Peru"};
+console.log(countries);
+
+//transforma los elementos de un objeto en un array
+console.log(Object.entries(countries));
---
+++ src/es8/01-objetc-values.js
@@ -0,0 +1,3 @@
+const countries = {MX: "Mexico", CO: "colombia", CL: "Chile", PE: "Peru"};
+//solo pasa los puros valores de cada uno de los objetos strings
+console.log(Object.values(countries));
