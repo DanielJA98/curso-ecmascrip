670d17b7bff06555a11a26d63f02af9b36717c1a
DanielJA98LDELLDeb <djimeneza7b2276@outlook.com>
2024-04-13 19:28:47 -0600

    clas 20 sting padding y trailing commas

---
+++ src/es8/03-string-padding.js
@@ -0,0 +1,7 @@
+const string = "Hello";
+
+//rellenas los espacios que le hacen falta segun lo indicado
+console.log(string.padStart(6, "h_i"));
+
+
+console.log(string.padEnd(6,"_"));
---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,4 @@
+const ages = [24, 25, 26, 32, 3, 12, 55, , , , ,];// , agrega items vacios
+
+console.log(ages);
+console.log(ages.length); //muestra la longitud del array
